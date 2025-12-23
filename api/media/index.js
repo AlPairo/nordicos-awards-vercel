a010ce9fa8d3b388d6cde423fc341cd2df6fd4f8
Manuel Perera <manuel.pererasb@gmail.com>
2025-12-23 14:02:16 -0300

    [FIX] Arreglar endpoints de /media

--- api/media/index.js
+++ api/media/index.js
@@ -35,6 +35,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
+}
 
 export default async function handler(req, res) {
     const { action, id } = req.query;
